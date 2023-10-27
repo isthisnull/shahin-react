d7b583f317baafe697d7bf986a01b401b494f186
isthisnull <letscodewithzanjiri@hotmail.com>
2023-10-28 00:24:14 +0300

    modal is ok

--- src/modal.jsx
+++ src/modal.jsx
@@ -10,24 +10,26 @@ export default function Modal() {
       <button onClick={toggleModal} className="btn-modal">
         Open
       </button>
-      <div className="modal">
-        <div onClick={toggleModal} className="overlay"></div>
-        <div className="modal-content">
-          <h2>Hello world</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-            debitis nobis, dolor doloremque aspernatur nihil totam impedit
-            maxime recusandae cupiditate officiis earum vel voluptate deserunt
-            asperiores pariatur sit vero temporibus! Magnam debitis iste nemo
-            aliquid at nesciunt, tempore reiciendis! Libero, et voluptatem odit
-            ab velit quas nisi optio ratione corrupti temporibus, placeat fugiat
-            iusto. Iure nobis sint harum debitis!
-          </p>
-          <button className="close-modal" onClick={toggleModal}>
-            Close
-          </button>
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Hello world</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              debitis nobis, dolor doloremque aspernatur nihil totam impedit
+              maxime recusandae cupiditate officiis earum vel voluptate deserunt
+              asperiores pariatur sit vero temporibus! Magnam debitis iste nemo
+              aliquid at nesciunt, tempore reiciendis! Libero, et voluptatem
+              odit ab velit quas nisi optio ratione corrupti temporibus, placeat
+              fugiat iusto. Iure nobis sint harum debitis!
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              Close
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
